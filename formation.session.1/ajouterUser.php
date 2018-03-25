6fbc627d1b8b3e3a927c35ecb0730788607300da
Thierry Bogusz <thbogusz@yhaoo.fr>
2018-03-25 20:25:18 +0200

    version 1 Ok ajouterUser.php avec csrf_2

---
+++ formation.session.1/ajouterUser.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * bienvenu.php
+ *
+ * @project	   formation.session
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Thierry Bogusz <thbogusz@yahoo.fr>
+*/
+$database=connectDb();
+ 
+$sql="insert into fs_user Values(null,?,?)";
+ 
+$param=array($_GET['user'],$_GET['pwd']);
+ 
+$sth=executeDb($database,$sql,$param);
+ 
+?>
+<h3>Un utilisateur a été ajouté par... <?php echo $_SESSION['user'];?></h3>
